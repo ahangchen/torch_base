97e2ebfde20a18286bb538ce567fa3c0cfbffd93
梦里茶 <1418659400@qq.com>
2021-09-14 09:46:41 +0800

    Update list_dataset.py

--- data/list_dataset.py
+++ data/list_dataset.py
@@ -4,9 +4,13 @@ import numpy as np
 
 
 class ListDataset(Dataset):
-    def __init__(self, args, data_list):
+    def __init__(self, args, is_train):
         # usually we need args rather than single datalist to init the dataset
         super(self, ListDataset).__init__()
+        if is_train:
+            data_list = args.train_list
+        else:
+            data_list = args.val_list
         infos = [line.split() for line in open(data_list).readlines()]
         self.img_paths = [info[0] for info in infos]
         self.label_paths = [infos[1] for info in infos]
